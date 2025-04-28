771ca0f8103c9b740e52bad88572faf99759e884
Solian21 <Soliancarlos@gmail.com>
2025-04-28 15:37:06 -0700

    Started the APi but I couldnt get it work just yet get it to work

--- app/src/main/java/Events_API.java
+++ app/src/main/java/Events_API.java
@@ -1,25 +1,17 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import serpapi.SerpApiSearchException;
+import serpapi.GoogleSearch;
+import java.util.Map;
+import java.util.HashMap;
+
 public class Events_API {
-    Map<String, String> parameter = new HashMap<>();
+    private String API_KEY = System.getenv("SERPAPI_KEY");
 
-    parameter.put("engine","google_events");
-    parameter.put("q","Events");
-    parameter.put("hl","en");
-    parameter.put("gl","in");
-    parameter.put("google_domain","google.co.in");
-    parameter.put("location","India");
-    parameter.put("api_key","secret_api_key");
+    public Events_API() {
 
-    GoogleSearch search = new GoogleSearch(parameter);
-
-try
-
-    {
-        JsonObject results = search.getJson();
-    } catch(
-    SerpApiSearchException ex)
-
-    {
-        System.out.println("Exception:");
-        System.out.println(ex.toString());
     }
 }
